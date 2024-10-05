c3aa11788decd430c85b6af100b9a70f28d33ead
Paul Webter <paul.gustav.webster@gmail.com>
2024-10-05 16:58:55 -0400

    Update Procfile and import routes in main.py

--- app/main.py
+++ app/main.py
@@ -5,11 +5,4 @@ from flask import Flask
 app = Flask(__name__)
 
 # Importing the routes from api_routes.py
-from .api_routes import *
-
-# The main entry point of the application
-if __name__ == '__main__':
-    # Running the Flask application
-    # debug=False means no debug information will be shown
-    # port=8000 specifies the port number on which the app will run
-    app.run(debug=False, port=8000)
+from routes.api_routes import *
--- app/routes/api_routes.py
+++ app/routes/api_routes.py
@@ -1,17 +1,9 @@
-# Importing necessary modules from Flask
 from flask import request, jsonify
-# Importing the Flask app instance from main.py
-from .main import app
-# Importing the webhook receiver function from webhook_receiver.py
-from .services.webhook_receiver import handle_webhook
+from main import app
+from services.webhook_receiver import handle_webhook
 
-# Defining a route for the webhook endpoint
-# This route will handle POST requests sent to /webhook
 @app.route('/webhook', methods=['POST'])
 def receive_webhook():
-    # Extracting JSON data from the incoming request
     data = request.json
-    # Calling the handle_webhook function to process the data
     response = handle_webhook(data)
-    # Returning the response as JSON with a status code of 200 (OK)
     return jsonify(response), 200
